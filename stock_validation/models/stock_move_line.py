dde37cdec02e3fec56c9943824a084f6258aa6f6
Tonny <corner.store59@gmail.com>
2024-03-04 21:45:39 -0600

    [FIX] La cantidad a dejar como hecho en la transferencia interna

--- stock_validation/models/stock_move_line.py
+++ stock_validation/models/stock_move_line.py
@@ -7,6 +7,22 @@ class StockMoveLine(models.Model):
 
     qty_on_hand = fields.Float(compute='compute_qty_on_hand', string='Cantidad disponible')
 
+    @api.model_create_multi
+    def create(self, vals):
+        for val in vals:
+            qty_done = val['qty_done']
+            product_id = self.env['product.product'].search([('id', '=', val['product_id'])])
+            location_id = self.env['stock.location'].search([('id', '=', val['location_id'])])
+            if qty_done != 0 and product_id and location_id:
+                available_qty = self.env["stock.quant"].\
+                    _get_available_quantity(product_id, location_id)
+                if available_qty <= 0:
+                    raise UserError(f"No hay en stock cantidad disponible de productos: "
+                                    f"{product_id.display_name}")
+                elif available_qty == qty_done or available_qty <= qty_done:
+                    val['qty_done'] = available_qty
+        return super(StockMoveLine, self).create(vals)
+
     @api.onchange('product_id', 'location_id')
     def compute_qty_on_hand(self):
         for picking_id in self:
