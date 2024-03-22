f6b45d9d38a6615148fd8e05bebc849abead4990
Tonny <corner.store59@gmail.com>
2024-03-22 08:16:38 -0600

    [FIX] Se arregla reporte por ubicaciones

--- stock_validation/models/stock_location.py
+++ stock_validation/models/stock_location.py
@@ -1,20 +1,33 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
-    show_in_report = fields.Boolean(compute='_compute_show_report', store=True, compute_sudo=True)
-    in_date = fields.Datetime(string='Fecha')
-    #
-    # def _compute_in_date(self):
-    #     for location_id in self:
-    #         picking_id = self.env['stock.picking'].search([
-    #             '|',
-    #             ('location_id', '=', location_id.id),
-    #             ('location_dest_id', '=', location_id.id),
-    #         ], order='create_date desc', limit=1)
-    #         location_id.in_date = picking_id.create_date
+    # show_in_report = fields.Boolean(compute='_compute_show_report', store=True, compute_sudo=True)
+    in_date = fields.Datetime(compute='_compute_in_date', string='Fecha', store=True, compute_sudo=True)
+    filter_init_date = fields.Datetime(string='Fecha filtrado inicial', default=False)
+    filter_end_date = fields.Datetime(string='Fecha filtrado final', default=False)
+
+    @api.model
+    def search_domain(self):
+        domain = [
+            ('complete_name', 'ilike', 'Holdc/stock%'),
+        ]
+        # if init_date_report:
+        #     domain.append(('in_date', '>=', init_date_report))
+        # if end_date_report:
+        #     domain.append(('in_date', '<=', end_date_report))
+        return domain
+
+    def _compute_in_date(self):
+        for location_id in self:
+            picking_id = self.env['stock.picking'].search([
+                '|',
+                ('location_id', '=', location_id.id),
+                ('location_dest_id', '=', location_id.id),
+            ], order='create_date desc', limit=1)
+            location_id.in_date = picking_id.create_date
     #
     # def _compute_show_report(self):
     #     for location_id in self:
--- stock_validation/wizard/stock_location_report_wizard.py
+++ stock_validation/wizard/stock_location_report_wizard.py
@@ -16,6 +16,8 @@ class StockLocationReportWizard(models.TransientModel):
             ('usage', '=', 'transit')
         ])
         for location_id in location_ids:
+            location_id.filter_init_date = self.init_date_report
+            location_id.filter_end_date = self.end_date_report
             picking_id = self.env['stock.picking'].search([
                 ('location_dest_id', '=', location_id.id),
                 ('state', '!=', 'cancel'),
@@ -24,7 +26,6 @@ class StockLocationReportWizard(models.TransientModel):
             ], order='create_date desc', limit=1)
             if picking_id:
                 location_id.in_date = picking_id.create_date
-        self = self.with_context(usage_location_ids=location_ids.ids)
         action = {
             'name': f'Ubicaciones sin movimiento: De la fecha'
                     f' {self.init_date_report.strftime("%d-%m-%Y")} a {self.end_date_report.strftime("%d-%m-%Y")}',
@@ -33,7 +34,16 @@ class StockLocationReportWizard(models.TransientModel):
             'view_id': self.env.ref('stock_validation.stock_location_report_tree').id,
             'res_model': 'stock.location',
             'type': 'ir.actions.act_window',
-            'domain': [('complete_name', 'ilike', 'Holdc/Stock%'), ('usage', '=', 'transit')],
+            'domain': [
+                '&',
+                ('complete_name', 'ilike', 'Holdc/Stock%'),
+                ('usage', '=', 'transit'),
+                '|',
+                '&',
+                ('in_date', '>=', self.init_date_report),
+                ('in_date', '<=', self.end_date_report),
+                ('in_date', '=', False),
+            ],
             'context': {'create': False},
         }
 
