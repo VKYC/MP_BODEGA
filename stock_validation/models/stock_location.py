6dcc3da35006705e7109e8804e57b16f50083a2b
Tonny <corner.store59@gmail.com>
2024-03-25 10:35:36 -0600

    [ADD] Se agrega vista de movimientos historicos de ubicacion

--- stock_validation/models/stock_location.py
+++ stock_validation/models/stock_location.py
@@ -4,20 +4,30 @@ from odoo import fields, models, api
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
-    # show_in_report = fields.Boolean(compute='_compute_show_report', store=True, compute_sudo=True)
     in_date = fields.Datetime(compute='_compute_in_date', string='Fecha', store=True, compute_sudo=True)
     filter_init_date = fields.Datetime(string='Fecha filtrado inicial', default=False)
     filter_end_date = fields.Datetime(string='Fecha filtrado final', default=False)
+
+    def get_history_moves(self):
+        action = {
+            'name': f'Hisotirial de movimientos',
+            'view_type': 'tree',
+            'view_mode': 'list',
+            'view_id': self.env.ref('stock_validation.history_moves_tree_view').id,
+            'res_model': 'stock.picking',
+            'type': 'ir.actions.act_window',
+            'domain': [
+                ('location_dest_id', '=', self.id),
+            ],
+            'context': {'create': False},
+        }
+        return action
 
     @api.model
     def search_domain(self):
         domain = [
             ('complete_name', 'ilike', 'Holdc/stock%'),
         ]
-        # if init_date_report:
-        #     domain.append(('in_date', '>=', init_date_report))
-        # if end_date_report:
-        #     domain.append(('in_date', '<=', end_date_report))
         return domain
 
     def _compute_in_date(self):
@@ -28,15 +38,3 @@ class StockLocation(models.Model):
                 ('location_dest_id', '=', location_id.id),
             ], order='create_date desc', limit=1)
             location_id.in_date = picking_id.create_date
-    #
-    # def _compute_show_report(self):
-    #     for location_id in self:
-    #         picking_ids = self.env['stock.picking'].search([
-    #             '|',
-    #             ('location_id', '=', location_id.id),
-    #             ('location_dest_id', '=', location_id.id),
-    #         ])
-    #         if not picking_ids:
-    #             location_id.show_in_report = True
-    #         else:
-    #             location_id.show_in_report = False
