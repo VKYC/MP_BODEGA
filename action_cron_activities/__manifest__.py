ee8d2edceb5160eca83f1a9ff005888e5cf0810d
Tonny <corner.store59@gmail.com>
2024-05-02 16:09:12 -0600

    [ADD] Scripts de actividades de pasar las actualizaciones en la segunda moneda

---
+++ action_cron_activities/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Action Cron Activities",
+
+    'summary': """Se generan scripts con Odoo""",
+
+    'author': "Tonny Velazquez",
+    'website': "corner.store59@gmail.com",
+
+    'category': 'Settings',
+    'version': '15.0.0.0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'data/ir_cron_data.xml',
+    ],
+}
