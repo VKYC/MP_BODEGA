22f59904edfad88586b18d9e24b52cc33deae64f
Tonny <corner.store59@gmail.com>
2024-05-04 15:25:27 -0600

    [ADD] Se agregan terminos y condiciones a los pedidos de venta y de ordenes de compra.

--- action_cron_activities/__manifest__.py
+++ action_cron_activities/__manifest__.py
@@ -9,10 +9,12 @@
     'category': 'Settings',
     'version': '15.0.0.0.1',
 
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', 'purchase', 'sale'],
 
     'data': [
         'data/ir_cron_data.xml',
         'views/external_layout_standard.xml',
+        'views/purchase_order_views.xml',
+        'views/sale_order_views.xml',
     ],
 }
