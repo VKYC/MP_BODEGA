e24f0edb1c4523ca3a0226934126f10c46530352
Tonny <corner.store59@gmail.com>
2024-05-13 17:08:09 -0600

    [DEL] Codigo innecesario

--- purchase_manage_custom/models/purchase_requisition.py
+++ purchase_manage_custom/models/purchase_requisition.py
@@ -2,34 +2,6 @@ from odoo import fields, models, api
 from odoo.exceptions import UserError
 
 
-class HrEmployeePublic(models.Model):
-    _inherit = 'hr.employee.public'
-
-    account_date = fields.Date(string='Fecha de cotabilizacion')
-    amount_account_auth = fields.Date(string='Fecha de cotabilizacion')
-    amount_account = fields.Date(string='Fecha de cotabilizacion')
-    company_currency_id = fields.Many2one(
-        comodel_name="res.currency",
-        string="Currency of the Payment Transaction",
-        required=True,
-        default=lambda self: self.env.user.company_id.currency_id,
-    )
-    move_id = fields.Many2one(
-        comodel_name="stock.move",
-        string="Move",
-    )
-    qty_to_approve = fields.Float(string='Cantidad a aprobar', default=0)
-    document_approve = fields.Selection(selection=[
-        ("solicitud_presupuesto", "Solicitudes de Presupuesto"),
-        ("orden_compra", "Orden de Compra")
-    ], string='Documento a Aprobar')
-    employee_bank_line_ids = fields.One2many(
-        comodel_name='employee.bank.line',
-        inverse_name='employee_id', string='Cuentas Bancarias'
-    )
-
-
-
 class PurchaseRequisition(models.Model):
     _inherit = 'purchase.requisition'
 
