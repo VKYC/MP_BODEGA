0df79084464e7fff3318e4aa26036e49376c76c1
Tonny <corner.store59@gmail.com>
2024-05-14 00:18:36 -0600

    [FIX] Tamanio y eliminacion de posibles errores en ir attachment

--- purchase_manage_custom/models/ir_attachment.py
+++ purchase_manage_custom/models/ir_attachment.py
@@ -10,16 +10,3 @@ class IrAttachment(models.Model):
         if self.user_has_groups("purchase_manage_custom.group_purchase_user_purchase"):
             raise UserError("No se puede eliminar registros.")
         return super(IrAttachment, self).unlink()
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        records = super(IrAttachment, self).create(vals_list)
-        if self.user_has_groups("purchase_manage_custom.group_purchase_user_purchase"):
-            raise UserError("No crear registros.")
-        return records
-
-    def write(self, vals):
-        if self.user_has_groups("purchase_manage_custom.group_purchase_user_purchase"):
-            raise UserError("No puede editar.")
-        res = super(IrAttachment, self).write(vals)
-        return res
