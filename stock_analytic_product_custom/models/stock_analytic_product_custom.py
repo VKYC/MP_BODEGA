46505c74891b537a781eddaa895dbe4e24c0129b
AdrianRHV <rascuas_29998@hotmail.com>
2024-05-24 16:26:59 -0600

    [ADD]First commit

---
+++ stock_analytic_product_custom/models/stock_analytic_product_custom.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class StockAnalyticProduct(models.Model):
+    _name = 'stock.analytic.product.custom'
+    _description = 'Stock Analytic Product Custom'
+
