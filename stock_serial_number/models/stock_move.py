da48101f55b04d91d2182270be7e68db5c85e16d
Tonny <corner.store59@gmail.com>
2024-06-13 11:24:55 -0600

    [FIX] Se arregla la condicion de las plantillas

--- stock_serial_number/models/stock_move.py
+++ stock_serial_number/models/stock_move.py
@@ -3,6 +3,7 @@ import openpyxl
 from io import BytesIO
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class StockMove(models.Model):
@@ -18,19 +19,22 @@ class StockMove(models.Model):
         self._get_rows(self.attachment_file, self.attachment_name)
 
     def _get_rows(self, attachment, attachment_name):
-        wb = openpyxl.load_workbook(filename=BytesIO(base64.b64decode(attachment)), read_only=True)
-        ws = wb.active
-        for record in ws.iter_rows(min_row=2, max_row=None, min_col=None,max_col=None, values_only=True):
-            if self.product_id.name == record[2]:
-                lot_id = self.env['stock.production.lot'].search([
-                    ('name', '=', record[0]),
-                    ('product_id', '=', self.product_id.id)
-                ], limit=1)
-                if lot_id:
-                    self.move_line_nosuggest_ids.create({
-                        'location_dest_id': self.location_dest_id.id,
-                        'product_uom_id': self.product_id.product_tmpl_id.uom_id.id,
-                        'product_id': self.product_id.id,
-                        'lot_id': lot_id.id,
-                        'qty_done': 1,
-                    })
+        try:
+            wb = openpyxl.load_workbook(filename=BytesIO(base64.b64decode(attachment)), read_only=True)
+            ws = wb.active
+            for record in ws.iter_rows(min_row=2, max_row=None, min_col=None,max_col=None, values_only=True):
+                if self.product_id.name == record[2]:
+                    lot_id = self.env['stock.production.lot'].search([
+                        ('name', '=', record[0]),
+                        ('product_id', '=', self.product_id.id)
+                    ], limit=1)
+                    if lot_id:
+                        self.move_line_nosuggest_ids.create({
+                            'location_dest_id': self.location_dest_id.id,
+                            'product_uom_id': self.product_id.product_tmpl_id.uom_id.id,
+                            'product_id': self.product_id.id,
+                            'lot_id': lot_id.id,
+                            'qty_done': 1,
+                        })
+        except Exception as e:
+            raise ValidationError('Sintaxis de la plantilla incorrecto')
